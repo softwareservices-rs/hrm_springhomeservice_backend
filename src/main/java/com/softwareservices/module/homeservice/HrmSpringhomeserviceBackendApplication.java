636adcfeda5579b5f8d65167cb3d695cdef16172
Kandula Rahul Reddy <kandularahulreddy96@gmail.com>
2024-01-09 14:26:23 +0530

    Create-a-repo-of-Spring-Boot-for-Human-Resource-Management

---
+++ src/main/java/com/softwareservices/module/homeservice/HrmSpringhomeserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.softwareservices.module.homeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmSpringhomeserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmSpringhomeserviceBackendApplication.class, args);
+	}
+
+}
